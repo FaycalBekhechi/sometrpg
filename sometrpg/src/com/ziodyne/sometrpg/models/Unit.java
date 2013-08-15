40f635f3a371faa3a45ed286bb233a8a15daadfd
Ryan Gee <ryan.cv.gee@gmail.com>
2013-08-14 22:16:29 -0400

    Modeling units
    hmm, AtomicLong seems correct here.

---
+++ sometrpg/src/com/ziodyne/sometrpg/models/Unit.java
@@ -0,0 +1,39 @@
+package com.ziodyne.sometrpg.models;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Unit {
+  private static final AtomicLong lastIdentifier = new AtomicLong(1L);
+  
+  private final long id;
+  private String name;
+  private Set<UnitStat> statSheet;
+  
+  public Unit(String name, Set<UnitStat> statSheet) {
+    super();
+    this.name = name;
+    this.statSheet = statSheet;
+    this.id = lastIdentifier.incrementAndGet();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<UnitStat> getStatSheet() {
+    return statSheet;
+  }
+
+  public void setStatSheet(Set<UnitStat> statSheet) {
+    this.statSheet = statSheet;
+  }
+
+  public long getId() {
+    return id;
+  }
+}

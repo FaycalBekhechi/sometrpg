ad5d4933991603b6c82fb3e1ed362fe9ebe8df9d
Ryan Gee <ryan.cv.gee@gmail.com>
2013-09-25 23:13:37 -0400

    Click/Tap to move implemented.

--- sometrpg/src/com/ziodyne/sometrpg/input/CameraMoveController.java
+++ sometrpg/src/com/ziodyne/sometrpg/input/CameraMoveController.java
@@ -1,12 +1,14 @@
 package com.ziodyne.sometrpg.input;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector3;
 
 public class CameraMoveController implements InputProcessor {
     private final OrthographicCamera camera;
+    private boolean ignoreNextTouchUp = false;
 
     public CameraMoveController(OrthographicCamera camera) {
         this.camera = camera;
@@ -28,20 +30,33 @@ public class CameraMoveController implements InputProcessor {
     }
 
     @Override
-    public boolean touchDown(int i, int i2, int i3, int i4) {
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean touchUp(int i, int i2, int i3, int i4) {
-        return false;
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+      if (button == Input.Buttons.LEFT && !ignoreNextTouchUp) {
+
+        // Get the touch position in world space.
+        Vector3 touchCoords3d = new Vector3(screenX, screenY, 1);
+        camera.unproject(touchCoords3d);
+
+        // Compute the offset from the current camera position
+        touchCoords3d = touchCoords3d.sub(camera.position);
+
+        camera.translate(touchCoords3d.x, touchCoords3d.y);
+      }
+      ignoreNextTouchUp = false;
+      return false;
     }
 
     @Override
     public boolean touchDragged(int i, int i2, int i3) {
         camera.translate(new Vector3(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY(), 0));
+        ignoreNextTouchUp = true;
 
-        return false;
+        return true;
     }
 
     @Override

da7e1947d388a5210f2be490fc59218ff6147aa7
Ryan Gee <ryan.cv.gee@gmail.com>
2013-09-26 02:19:16 -0400

    Removing unnecessary camera.

--- sometrpg/src/com/ziodyne/sometrpg/SomeTRPG.java
+++ sometrpg/src/com/ziodyne/sometrpg/SomeTRPG.java
@@ -6,12 +6,9 @@ import com.ziodyne.sometrpg.screens.Splash;
 import com.ziodyne.sometrpg.screens.TestBattle;
 
 public class SomeTRPG extends Game {
-  private OrthographicCamera cam;
-  
+
 	@Override
 	public void create() {
-		cam = new OrthographicCamera();
-		cam.setToOrtho(false, 840, 400);
 		setScreen(new TestBattle(this));
 	}
 }

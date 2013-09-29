88f1fcb9979fb2544f952eb165ab3320bfd57528
Ryan Gee <ryan.cv.gee@gmail.com>
2013-09-29 12:58:24 -0400

    Render the test chart
    Hard-coded test indices and vertices for now.

--- core/src/main/java/com/ziodyne/sometrpg/SomeTRPG.java
+++ core/src/main/java/com/ziodyne/sometrpg/SomeTRPG.java
@@ -3,6 +3,7 @@ package com.ziodyne.sometrpg;
 import com.badlogic.gdx.Game;
 import com.ziodyne.sometrpg.screens.ChartTestScreen;
 import com.ziodyne.sometrpg.screens.MainMenu;
+import com.ziodyne.sometrpg.screens.TestBattle;
 
 public class SomeTRPG extends Game {
 
--- core/src/main/java/com/ziodyne/sometrpg/screens/ChartTestScreen.java
+++ core/src/main/java/com/ziodyne/sometrpg/screens/ChartTestScreen.java
@@ -2,9 +2,11 @@ package com.ziodyne.sometrpg.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
@@ -12,24 +14,43 @@ import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 
 public class ChartTestScreen implements Screen {
   private Mesh mesh;
+  private OrthographicCamera camera;
   
   public ChartTestScreen() {
+    camera = new OrthographicCamera(800, 400);
+    camera.translate(0, 0);
+
     MeshBuilder builder = new MeshBuilder();
     builder.begin(new VertexAttributes(new VertexAttribute(Usage.Position, 3, "a_position"),
                                        new VertexAttribute(Usage.ColorPacked, 4, "a_color")));
-    
-    builder.vertex(new float[]{ -0.5f, -0.5f, 0, Color.toFloatBits(255, 0, 0, 255) });
-    builder.vertex(new float[]{ 0.5f, -0.5f, 0, Color.toFloatBits(0, 255, 0, 255)});
-    builder.vertex(new float[]{ 0, 0.5f, 0, Color.toFloatBits(0, 0, 255, 255) });
 
-    
+    builder.vertex(new float[]{ 0, 0, 0, Color.toFloatBits(0, 0, 255, 255) });
+    builder.vertex(new float[]{ 0, 2, 0, Color.toFloatBits(255, 0, 0, 255) });
+    builder.vertex(new float[]{ -2, 1, 0, Color.toFloatBits(0, 255, 0, 255)});
+    builder.vertex(new float[]{ -2, -1, 0, Color.toFloatBits(0, 0, 255, 255) });
+    builder.vertex(new float[]{ 0, -2, 0, Color.toFloatBits(0, 255, 0, 255) });
+    builder.vertex(new float[]{ 2, -1, 0, Color.toFloatBits(255, 0, 0, 255) });
+    builder.vertex(new float[]{ 2, 1, 0, Color.toFloatBits(0, 255, 0, 255) });
+
+    builder.index((short)0, (short)1, (short)2);
+    builder.index((short)0, (short)2, (short)3);
+    builder.index((short)0, (short)3, (short)4);
+    builder.index((short)0, (short)4, (short)5);
+    builder.index((short)0, (short)5, (short)6);
+    builder.index((short)0, (short)6, (short)1);
+
+
     mesh = builder.end();
   }
   
   @Override
   public void render(float delta) {
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-    mesh.render(GL10.GL_TRIANGLES);
+
+    camera.update();
+    camera.zoom = 0.01f;
+    camera.apply(Gdx.gl10);
+    mesh.render(GL10.GL_TRIANGLE_FAN);
   }
 
   @Override

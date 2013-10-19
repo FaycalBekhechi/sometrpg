e130a1af01d72bfa6bf4149a047d92bd243f6cac
Ryan Gee <ryan.cv.gee@gmail.com>
2013-10-19 15:52:51 -0400

    Disable the power of 2 texture size req on desktop

--- desktop/src/main/java/com/ziodyne/sometrpg/Main.java
+++ desktop/src/main/java/com/ziodyne/sometrpg/Main.java
@@ -2,16 +2,18 @@ package com.ziodyne.sometrpg;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Main {
  public static void main(String[] args) {
    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
    cfg.title = "sometrpg";
    cfg.useGL20 = false;
-   cfg.width = 480;
-   cfg.height = 320;
+   cfg.width = 1600;
+   cfg.height = 900;
    cfg.samples = 8;
-   
+
+   Texture.setEnforcePotImages(false);
    new LwjglApplication(new SomeTRPG(), cfg);
  }
 }

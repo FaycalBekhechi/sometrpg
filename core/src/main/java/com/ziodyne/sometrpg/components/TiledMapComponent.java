1caa45868a549315c01972346ac2e9c36190176a
Ryan Gee <ryan.cv.gee@gmail.com>
2013-10-19 15:07:57 -0400

    Add a Tiled map renderer and component
    This got a bit awkward because map renderers from LibGDX are 1-1 with
    maps, so we cannot reuse a renderer for all maps. (Really there should
    only be one, but Artemis forces us to divorce the map and its renderer).
    Thus, the map component has a renderer. It uses the global SpriteBatch.

---
+++ core/src/main/java/com/ziodyne/sometrpg/components/TiledMapComponent.java
@@ -0,0 +1,27 @@
+package com.ziodyne.sometrpg.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+/**
+ * An Artemis component that holds a Map instance loaded from Tiled.
+ */
+public class TiledMapComponent extends Component {
+  private final TiledMap map;
+  private final OrthogonalTiledMapRenderer renderer;
+
+  public TiledMapComponent(TiledMap map, SpriteBatch batch) {
+    this.map = map;
+    this.renderer = new OrthogonalTiledMapRenderer(map, batch);
+  }
+
+  public TiledMap getMap() {
+    return map;
+  }
+
+  public OrthogonalTiledMapRenderer getRenderer() {
+    return renderer;
+  }
+}

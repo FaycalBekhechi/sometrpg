32e001704682c12c0740464433d61a66da0fd5c9
Ryan Gee <rgee@squarespace.com>
2013-11-28 12:18:17 -0500

    MainMenu uses the screen adapter class to reduce boilerplate.

--- core/src/main/java/com/ziodyne/sometrpg/view/screens/MainMenu.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/screens/MainMenu.java
@@ -9,6 +9,7 @@ import aurelienribon.tweenengine.TweenManager;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -19,7 +20,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.ziodyne.sometrpg.view.tween.ActorAccessor;
 
-public class MainMenu implements Screen {
+public class MainMenu extends ScreenAdapter {
   private final Stage stage;
   private final Skin skin;
   private final Game game;
@@ -115,29 +116,6 @@ public class MainMenu implements Screen {
   @Override
   public void resize(int width, int height) {
     stage.setViewport(width, height, true);
-  }
-
-  @Override
-  public void show() {
-
-  }
-
-  @Override
-  public void hide() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void pause() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void resume() {
-    // TODO Auto-generated method stub
-
   }
 
   @Override

6b7ea8e2b100f2386f4f1d5cc96fef0c99e59554
Ryan Gee <rgee@squarespace.com>
2013-12-07 16:50:54 -0500

    Implement an untested version of A* pathfinding. Generalized for any space.

--- core/src/main/java/com/ziodyne/sometrpg/logic/navigation/Path.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/navigation/Path.java
@@ -5,27 +5,27 @@ import com.google.common.collect.Lists;
 
 import java.util.List;
 
-public class Path {
-  private List<GridPoint2> points;
+public class Path<T> {
+  private List<T> points;
 
-  public static class Builder {
-    private List<GridPoint2> points = Lists.newArrayList();
+  public static class Builder<T> {
+    private List<T> points = Lists.newArrayList();
 
-    public Builder addPoint(GridPoint2 point) {
+    public Builder<T> addPoint(T point) {
       points.add(point);
       return this;
     }
 
-    public Path build() {
-      return new Path(points);
+    public Path<T> build() {
+      return new Path<T>(points);
     }
   }
 
-  public Path(List<GridPoint2> points) {
+  public Path(List<T> points) {
     this.points = points;
   }
 
-  public List<GridPoint2> getPoints() {
+  public List<T> getPoints() {
     return points;
   }
 }
--- core/src/main/java/com/ziodyne/sometrpg/logic/navigation/Pathfinder.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/navigation/Pathfinder.java
@@ -3,23 +3,40 @@ package com.ziodyne.sometrpg.logic.navigation;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Comparator;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
+
 public class Pathfinder<T> {
+  // TODO: Wrap these in an interface
   private final Function<T, Double> heuristicFunction;
   private final Function<T, Double> trueCostFunction;
+  private Function<T, Double> movementCostFunction;
   private Function<T, Set<T>> getNeighbors;
+  private Function<Pair<T, T>, Double> distanceFunction;
   private final Predicate<T> goalCheckFunction;
 
   private class NodeCostComparator implements Comparator<T> {
     @Override
     public int compare(T lhs, T rhs) {
-      return computeCost(lhs).compareTo(computeCost(rhs));
+      // TODO: Cache these?
+      double lCost = computeCost(lhs);
+      double rCost = computeCost(rhs);
+
+      if (lCost < rCost) {
+        return -1;
+      } else if (lCost > rCost) {
+        return 1;
+      }
+
+      return 0;
     }
   }
 
@@ -29,37 +46,92 @@ public class Pathfinder<T> {
     this.goalCheckFunction = goalCheckFunction;
   }
 
-  public Optional<Path> computePath(T start, T goal) {
+  public Optional<Path<T>> computePath(T start, T goal) {
     Set<T> closedNodes = Sets.newHashSet();
+    Map<T, Double> exactCosts = Maps.newHashMap();
+    Map<T, Double> estimatedCosts = Maps.newHashMap();
+    Map<T, T> breadcrumbs = Maps.newHashMap();
     Queue<T> openNodes = new PriorityQueue<T>(1, new NodeCostComparator());
     openNodes.add(start);
 
+    exactCosts.put(start, 0d);
+    estimatedCosts.put(start, estimatedCost(start));
 
-    while (!goalCheckFunction.apply(openNodes.peek())) {
+
+    while (!openNodes.isEmpty()) {
       T currentCheapest = openNodes.poll();
+      if (isGoal(currentCheapest)) {
+        return Optional.of(constructPath(breadcrumbs, goal));
+      }
+
+      openNodes.remove(currentCheapest);
       closedNodes.add(currentCheapest);
-
       Set<T> neighbors = getNeighbors.apply(currentCheapest);
-      Double trueCost = trueCostFunction.apply(currentCheapest);
-      for(T neighbor : neighbors) {
+      for (T neighbor : neighbors) {
+        double neighborCost = exactCosts.get(currentCheapest) + distance(currentCheapest, neighbor);
+        double heuristicScore = neighborCost + estimatedCost(neighbor);
+        Double previousEstimate = estimatedCosts.get(neighbor);
 
+        if (closedNodes.contains(neighbor) && heuristicScore >= previousEstimate) {
+          continue;
+        }
+
+        if (!openNodes.contains(neighbor) || heuristicScore > previousEstimate) {
+          breadcrumbs.put(neighbor, currentCheapest);
+          exactCosts.put(neighbor, neighborCost);
+          estimatedCosts.put(neighbor, heuristicScore);
+          openNodes.add(neighbor);
+        }
       }
     }
 
     return Optional.absent();
   }
 
-  private Double computeCost(T node) {
-    Double costEstimate = heuristicFunction.apply(node);
-    if (costEstimate == null) {
-      throw new IllegalArgumentException("Heuristic function returned null.");
+  private Path<T> constructPath(Map<T, T> breadcrumbs, T currentNode) {
+    return constructPath(breadcrumbs, new Path.Builder<T>(), currentNode);
+  }
+
+  private Path<T> constructPath(Map<T, T> breadCrumbs, Path.Builder<T> pathBuilder, T currentNode) {
+    if (breadCrumbs.containsKey(currentNode)) {
+      return constructPath(breadCrumbs, pathBuilder.addPoint(currentNode), breadCrumbs.get(currentNode));
+    } else {
+      return pathBuilder.build();
+    }
+  }
+
+  private double distance(T start, T end) {
+    Double distance = distanceFunction.apply(Pair.of(start, end));
+    if (distance == null) {
+      throw new IllegalArgumentException("Distance function returned null.");
     }
 
+    return distance;
+  }
+
+  private boolean isGoal(T node) {
+    return goalCheckFunction.apply(node);
+  }
+
+  private double trueCost(T node) {
     Double trueCost = trueCostFunction.apply(node);
     if (trueCost == null) {
       throw new IllegalArgumentException("True cost function returned null.");
     }
 
-    return costEstimate + trueCost;
+    return trueCost;
+  }
+
+  private double estimatedCost(T node) {
+    Double costEstimate = heuristicFunction.apply(node);
+    if (costEstimate == null) {
+      throw new IllegalArgumentException("Heuristic function returned null.");
+    }
+
+    return costEstimate;
+  }
+
+  private double computeCost(T node) {
+    return estimatedCost(node) + trueCost(node);
   }
 }

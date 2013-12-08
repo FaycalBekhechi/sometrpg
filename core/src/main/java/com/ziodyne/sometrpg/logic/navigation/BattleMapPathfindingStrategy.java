81d589149a5119455e10b34150897e9ed175e85a
Ryan Gee <rgee@squarespace.com>
2013-12-07 21:35:27 -0500

    Make the map pathfinding strategy aware of the map and of the passability of tiles.

--- core/src/main/java/com/ziodyne/sometrpg/logic/navigation/BattleMapPathfindingStrategy.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/navigation/BattleMapPathfindingStrategy.java
@@ -2,10 +2,18 @@ package com.ziodyne.sometrpg.logic.navigation;
 
 import com.badlogic.gdx.math.GridPoint2;
 import com.google.common.collect.Sets;
+import com.ziodyne.sometrpg.logic.models.battle.BattleMap;
+import com.ziodyne.sometrpg.logic.models.battle.Tile;
 
 import java.util.Set;
 
-public class MapPathfindingStrategy implements PathfindingStrategy<GridPoint2> {
+public class BattleMapPathfindingStrategy implements PathfindingStrategy<GridPoint2> {
+  private final BattleMap map;
+
+  public BattleMapPathfindingStrategy(BattleMap map) {
+    this.map = map;
+  }
+
   @Override
   public double estimateCost(GridPoint2 node, GridPoint2 goal) {
     int dx = Math.abs(node.x - goal.x);
@@ -16,6 +24,16 @@ public class MapPathfindingStrategy implements PathfindingStrategy<GridPoint2> {
 
   @Override
   public double distance(GridPoint2 start, GridPoint2 end) {
+    Tile endTile = map.getTile(end.x, end.y);
+
+    // Give this move a maximally high cost if the unit can't move there for any of the following reasons:
+    //   - It's a tile not on the grid
+    //   - The tile is occupied by another unit
+    //   - The tile is marked as impassable
+    if (endTile == null || endTile.isOccupied() || !endTile.isPassable()) {
+      return Double.MAX_VALUE;
+    }
+
     return Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2));
   }
 

1e0037f320c5c3397868959e2b5026fa5098c12d
Ryan Gee <ryan.cv.gee@gmail.com>
2013-12-24 21:09:16 -0500

    Briefly document the point of the RangeFinder interface.

--- core/src/main/java/com/ziodyne/sometrpg/logic/navigation/RangeFinder.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/navigation/RangeFinder.java
@@ -5,6 +5,11 @@ import com.ziodyne.sometrpg.logic.models.battle.BattleMap;
 
 import java.util.Set;
 
+/**
+ * A range finder computes movement or attack ranges on a map, from a starting square.
+ *
+ * The implementation may define exactly how the resulting squares are chosen.
+ */
 public interface RangeFinder {
   public Set<GridPoint2> computeRange(BattleMap map, GridPoint2 start, int maxDistance);
 }

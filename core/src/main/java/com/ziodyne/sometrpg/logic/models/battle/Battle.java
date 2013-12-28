9e5cfef4afce19d6f3aebbdedc873a389133a169
Ryan Gee <ryan.cv.gee@gmail.com>
2013-12-28 00:11:39 -0500

    Documenting the Battle interface.

--- core/src/main/java/com/ziodyne/sometrpg/logic/models/battle/Battle.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/models/battle/Battle.java
@@ -6,13 +6,62 @@ import com.ziodyne.sometrpg.logic.models.battle.combat.Combatant;
 import java.util.Set;
 
 public interface Battle {
+  /**
+   * Returns the {@link Army} active for the current turn.
+   *
+   * @return the active Army for the current turn
+   */
   public Army getCurrentTurnArmy();
+
+  /**
+   * Returns the {@link Combatant}s owned by the player.
+   *
+   * @return the Combatants owned by the player
+   */
   public Set<Combatant> getPlayerUnits();
+
+  /**
+   * Returns the {@link Combatant}s owned by the enemy.
+   *
+   * @return ths Combatants owned by the enemy
+   */
   public Set<Combatant> getEnemyUnits();
+
+  /**
+   * Returns the {@link Combatant}s owned by neither the enemy nor the player.
+   *
+   * @return the Combatants owned by neither the enemy nor the player
+   */
   public Set<Combatant> getNeutralUnits();
+
+  /**
+   * Move a {@link Combatant} to another {@link Tile}.
+   *
+   * @param combatant The Combatant being moved
+   * @param destination The Tile to which to move them
+   */
   public void moveCombatant(Combatant combatant, Tile destination);
+
+  /**
+   * Have one {@link Combatant} attack another.
+   *
+   * @param attacker The Combatant attacking
+   * @param attack The {@link Attack} to perform
+   * @param defender The Combatant receiving the attack
+   */
   public void executeAttack(Combatant attacker, Attack attack, Combatant defender);
 
+  /**
+   * Return <tt>true</tt> if the battle is won.
+   *
+   * @return <tt>true</tt> if the battle is won
+   */
   public boolean isWon();
+
+  /**
+   * Return <tt>true</tt> if the battle is lost.
+   *
+   * @return <tt>true</tt> if the battle is lost
+   */
   public boolean isLost();
 }

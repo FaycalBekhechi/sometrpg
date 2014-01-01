6e521f2ae24c305fea7f3c28539d663dc6cf255c
Ryan Gee <ryan.cv.gee@gmail.com>
2014-01-01 16:38:06 -0500

    Add Jackson

--- core/src/main/java/com/ziodyne/sometrpg/view/assets/Asset.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/assets/Asset.java
@@ -1,11 +1,35 @@
 package com.ziodyne.sometrpg.view.assets;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import static com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @Type(value = TextureAsset.class, name = "texture"),
+        @Type(value = TiledMapAsset.class, name = "tiledMap")
+})
 public abstract class Asset<T> {
   private String type;
   private String filename;
+  private Class<T> clazz;
 
   protected Asset(String type, Class<T> clazz) {
     this.type = type;
+    this.clazz = clazz;
+  }
+
+  public Class<T> getClazz() {
+    return clazz;
+  }
+
+  public void setClazz(Class<T> clazz) {
+    this.clazz = clazz;
   }
 
   public String getType() {

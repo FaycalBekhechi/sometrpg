8bae8302b5bdc7c1b5cdeb1bcf9cf882a16bda65
Ryan Gee <ryan.cv.gee@gmail.com>
2014-01-12 02:20:27 -0500

    Actually fix and prettify the flood filler.

--- core/src/main/java/com/ziodyne/sometrpg/logic/navigation/FloodFillRangeFinder.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/navigation/FloodFillRangeFinder.java
@@ -12,46 +12,51 @@ import java.util.Stack;
 public class FloodFillRangeFinder implements RangeFinder {
   @Override
   public Set<GridPoint2> computeRange(BattleMap map, GridPoint2 start, int maxDistance) {
-    Set<Equivalence.Wrapper<GridPoint2>> results = Sets.newHashSet();
-
-    Stack<GridPoint2> nodes = new Stack<GridPoint2>();
-    nodes.push(start);
-
-    while (!nodes.isEmpty()) {
-      GridPoint2 currentNode = nodes.pop();
-      results.add(MathUtils.GRID_POINT_EQUIV.wrap(currentNode));
-
-      GridPoint2 west = MathUtils.getWestNeighbor(currentNode);
-      Equivalence.Wrapper<GridPoint2> wrappedWest = MathUtils.GRID_POINT_EQUIV.wrap(west);
-      if (!results.contains(wrappedWest) && map.isPassable(west.x, west.y) && MathUtils.manhattanDistance(start, west) < maxDistance) {
-        nodes.push(west);
-      }
-
-      GridPoint2 east = MathUtils.getEastNeighbor(currentNode);
-      Equivalence.Wrapper<GridPoint2> wrappedEast = MathUtils.GRID_POINT_EQUIV.wrap(east);
-      if (!results.contains(wrappedEast) && map.isPassable(east.x, east.y) && MathUtils.manhattanDistance(start, east) < maxDistance) {
-        nodes.push(east);
-      }
-
-      GridPoint2 north = MathUtils.getNorthNeighbor(currentNode);
-      Equivalence.Wrapper<GridPoint2> wrappedNorth = MathUtils.GRID_POINT_EQUIV.wrap(north);
-      if (!results.contains(wrappedNorth) && map.isPassable(north.x, north.y) && MathUtils.manhattanDistance(start, north) < maxDistance) {
-        nodes.push(north);
-      }
-
-      GridPoint2 south = MathUtils.getSouthNeighbor(currentNode);
-      Equivalence.Wrapper<GridPoint2> wrappedSouth = MathUtils.GRID_POINT_EQUIV.wrap(south);
-      if (!results.contains(wrappedSouth) && map.isPassable(south.x, south.y) && MathUtils.manhattanDistance(start, south) < maxDistance) {
-        nodes.push(south);
-      }
-    }
-
     Set<GridPoint2> finalResults = Sets.newHashSet();
-    for (Equivalence.Wrapper<GridPoint2> point2Wrapper : results) {
-      GridPoint2 point = point2Wrapper.get();
+    Set<Equivalence.Wrapper<GridPoint2>> results = floodFill(map, start, maxDistance);
+
+    for (Equivalence.Wrapper<GridPoint2> gridPointWrapper : results) {
+      GridPoint2 point = gridPointWrapper.get();
       finalResults.add(point);
     }
 
     return finalResults;
+  }
+
+  private Set<Equivalence.Wrapper<GridPoint2>> floodFill(BattleMap map, GridPoint2 start, int maxDistance) {
+    Set<Equivalence.Wrapper<GridPoint2>> results = Sets.newHashSet();
+
+    Stack<GridPoint2> fringe = new Stack<GridPoint2>();
+    fringe.push(start);
+
+    while (!fringe.isEmpty()) {
+      GridPoint2 currentNode = fringe.pop();
+      results.add(wrapEquiv(currentNode));
+
+      for (GridPoint2 neighbor : getNeighbors(currentNode)) {
+        boolean unseen = !results.contains(wrapEquiv(neighbor));
+        boolean passable = map.isPassable(neighbor.x, neighbor.y);
+        boolean inRange = MathUtils.manhattanDistance(start, neighbor) <= maxDistance;
+
+        if (unseen && passable && inRange) {
+          fringe.push(neighbor);
+        }
+      }
+    }
+
+    return results;
+  }
+
+  private static Equivalence.Wrapper<GridPoint2> wrapEquiv(GridPoint2 point) {
+    return MathUtils.GRID_POINT_EQUIV.wrap(point);
+  }
+
+  private static Set<GridPoint2> getNeighbors(GridPoint2 start) {
+    return Sets.newHashSet(
+      MathUtils.getEastNeighbor(start),
+      MathUtils.getWestNeighbor(start),
+      MathUtils.getNorthNeighbor(start),
+      MathUtils.getSouthNeighbor(start)
+    );
   }
 }

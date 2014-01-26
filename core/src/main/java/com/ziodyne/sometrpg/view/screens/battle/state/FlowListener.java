fb487b231153b16e4f7f896bb55123ffbc155781
Ryan Gee <ryan.cv.gee@gmail.com>
2014-01-26 14:59:32 -0500

    FlowListener now allows you to share state between whenEnter/whenLeave transition handlers.

--- core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/FlowListener.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/FlowListener.java
@@ -1,7 +1,37 @@
 package com.ziodyne.sometrpg.view.screens.battle.state;
 
 import au.com.ds.ef.EasyFlow;
+import au.com.ds.ef.StateEnum;
+import au.com.ds.ef.StatefulContext;
+import au.com.ds.ef.call.ContextHandler;
 
-public interface FlowListener {
-  public void bind(EasyFlow<BattleContext> flow);
+/**
+ * An abstract class to ease the sharing of state between onEnter/onLeave event handlers.
+ * @param <T>
+ */
+public abstract class FlowListener<T extends StatefulContext> {
+  private final StateEnum state;
+
+  protected FlowListener(StateEnum state) {
+    this.state = state;
+  }
+
+  public abstract void onLeave(T conext);
+  public abstract void onEnter(T context);
+
+  public void bind(EasyFlow<T> flow) {
+    flow.whenEnter(state, new ContextHandler<T>() {
+      @Override
+      public void call(T context) throws Exception {
+        onEnter(context);
+      }
+    });
+
+    flow.whenLeave(state, new ContextHandler<T>() {
+      @Override
+      public void call(T context) throws Exception {
+        onLeave(context);
+      }
+    });
+  }
 }

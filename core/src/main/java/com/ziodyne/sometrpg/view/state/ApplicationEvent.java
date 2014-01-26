f43af796d81dd8afb8cd268cb0bc3f72cffc9a3f
Ryan Gee <ryan.cv.gee@gmail.com>
2014-01-25 20:01:23 -0500

    Begin to describe the FSM for application lifecycle.

---
+++ core/src/main/java/com/ziodyne/sometrpg/view/state/ApplicationEvent.java
@@ -0,0 +1,7 @@
+package com.ziodyne.sometrpg.view.state;
+
+import au.com.ds.ef.EventEnum;
+
+public enum ApplicationEvent implements EventEnum {
+  GAME_START
+}
---
+++ core/src/main/java/com/ziodyne/sometrpg/view/state/ApplicationState.java
@@ -0,0 +1,8 @@
+package com.ziodyne.sometrpg.view.state;
+
+import au.com.ds.ef.StateEnum;
+
+public enum ApplicationState implements StateEnum {
+  MAIN_MENU,
+  IN_BATTLE
+}

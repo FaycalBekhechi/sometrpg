c5e01b7185cac43ffa6d04b9c8efc2222fb38287
Ryan Gee <ryan.cv.gee@gmail.com>
2014-02-02 18:02:29 -0500

    Implement some debug logging for state flow listeners.

--- core/src/main/java/com/ziodyne/sometrpg/view/SomeTRPG.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/SomeTRPG.java
@@ -1,6 +1,8 @@
 package com.ziodyne.sometrpg.view;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.ziodyne.sometrpg.RootModule;
@@ -10,9 +12,9 @@ public class SomeTRPG extends Game {
 
   @Override
 	public void create() {
+    Gdx.app.setLogLevel(Application.LOG_DEBUG);
     Injector injector = Guice.createInjector(new RootModule(this));
     Director director = injector.getInstance(Director.class);
-
     director.addScreen(injector.getInstance(MainMenu.class));
 		//setScreen(new TestBattle());
 	  //setScreen(new ChartTestScreen());
--- core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/FlowListener.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/FlowListener.java
@@ -5,13 +5,17 @@ import au.com.ds.ef.StateEnum;
 import au.com.ds.ef.StatefulContext;
 import au.com.ds.ef.call.ContextHandler;
 import au.com.ds.ef.err.LogicViolationError;
+import com.ziodyne.sometrpg.logging.GdxLogger;
+import com.ziodyne.sometrpg.logging.Logger;
 
 /**
  * An abstract class to ease the sharing of state between onEnter/onLeave event handlers.
  * @param <T>
  */
 public abstract class FlowListener<T extends StatefulContext> {
+  public static boolean DEBUG = true;
   private final StateEnum state;
+  private final Logger LOG = new GdxLogger(this.getClass());
 
   protected FlowListener(StateEnum state) {
     this.state = state;
@@ -24,6 +28,9 @@ public abstract class FlowListener<T extends StatefulContext> {
     flow.whenEnter(state, new ContextHandler<T>() {
       @Override
       public void call(T context) throws Exception {
+        if (DEBUG) {
+          LOG.debug("Entering state: " + state);
+        }
         onEnter(context);
       }
     });
@@ -31,6 +38,9 @@ public abstract class FlowListener<T extends StatefulContext> {
     flow.whenLeave(state, new ContextHandler<T>() {
       @Override
       public void call(T context) throws Exception {
+        if (DEBUG) {
+          LOG.debug("Leaving state: " + state);
+        }
         onLeave(context);
       }
     });

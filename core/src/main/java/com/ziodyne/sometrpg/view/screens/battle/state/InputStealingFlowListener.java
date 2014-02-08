b4579943259e578d5e5993c06e5c69048dbe7b24
Ryan Gee <ryan.cv.gee@gmail.com>
2014-02-08 17:56:06 -0500

    Document InputStealingFlowListener

--- core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/InputStealingFlowListener.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/screens/battle/state/InputStealingFlowListener.java
@@ -6,6 +6,10 @@ import au.com.ds.ef.err.LogicViolationError;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 
+/**
+ * A flow listener that usurps some general, systemwide input processor and cleanly reinstalls it
+ * when the state is exited.
+ */
 public class InputStealingFlowListener<T extends StatefulContext> extends FlowListener<T> {
   private InputProcessor previousInputProcessor;
 

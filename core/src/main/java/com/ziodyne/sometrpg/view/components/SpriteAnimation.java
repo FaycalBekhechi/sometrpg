cb791894fad39f3bb79b1dfe4e0fe78996ac39bf
Ryan Gee <ryan.cv.gee@gmail.com>
2014-04-02 22:17:12 -0400

    Documenting sprite animation component.

--- core/src/main/java/com/ziodyne/sometrpg/view/components/SpriteAnimation.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/components/SpriteAnimation.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import org.apache.commons.lang3.Validate;
 
+/**
+ * This class is an Artemis {@link com.artemis.Component} that holds an {@link com.badlogic.gdx.graphics.g2d.Animation}.
+ */
 public class SpriteAnimation extends Component {
 
   private final Animation animation;

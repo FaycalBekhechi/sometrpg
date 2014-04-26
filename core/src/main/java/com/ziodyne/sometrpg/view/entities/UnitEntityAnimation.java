34788980b04701d69921a318b04bf323cd9678c6
Ryan Gee <ryan.cv.gee@gmail.com>
2014-04-26 14:04:13 -0400

    Quick docs on UnitEntityAnimation.

--- core/src/main/java/com/ziodyne/sometrpg/view/entities/UnitEntityAnimation.java
+++ core/src/main/java/com/ziodyne/sometrpg/view/entities/UnitEntityAnimation.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.ziodyne.sometrpg.logic.loader.models.AnimationSpec;
 import com.ziodyne.sometrpg.view.AnimationType;
 
+/**
+ * A bundle class for a single unit entity's animation.
+ */
 public class UnitEntityAnimation {
   private final Texture texture;
   private final AnimationType type;

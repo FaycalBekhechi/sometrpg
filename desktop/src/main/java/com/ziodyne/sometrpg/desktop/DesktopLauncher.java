af2b5fb25b663487fcea4e9b0c772e0e1d73c748
Ryan Gee <ryan.cv.gee@gmail.com>
2014-06-22 09:22:48 -0400

    Use new libgdx-generated gradle files with minor tweaks...basically had to shuffle every file.

--- desktop/src/main/java/com/ziodyne/sometrpg/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/ziodyne/sometrpg/desktop/DesktopLauncher.java
@@ -1,11 +1,11 @@
-package com.ziodyne.sometrpg;
+package com.ziodyne.sometrpg.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Texture;
 import com.ziodyne.sometrpg.view.SomeTRPG;
 
-public class Main {
+public class DesktopLauncher {
  public static void main(String[] args) {
    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
    cfg.title = "sometrpg";

f8a0fd0164b46a149a25a2f35e8c48b345762baf
Ryan Gee <ryan.cv.gee@gmail.com>
2014-07-05 14:55:00 -0400

    Add max stats to character spec.

--- core/src/main/java/com/ziodyne/sometrpg/logic/loader/models/CharacterSpec.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/loader/models/CharacterSpec.java
@@ -3,12 +3,18 @@ package com.ziodyne.sometrpg.logic.loader.models;
 public class CharacterSpec {
   private String id;
   private String name;
+  private CharacterStats maxStats;
   private CharacterStats stats;
   private CharacterGrowths growths;
 
   public String getId() {
 
     return id;
+  }
+
+  public CharacterStats getMaxStats() {
+
+    return maxStats;
   }
 
   public String getName() {

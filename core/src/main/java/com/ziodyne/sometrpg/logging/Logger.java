03b0997f47db1a40f5af6f4dad1e1ce6dbf98016
Ryan Gee <ryan.cv.gee@gmail.com>
2014-08-13 22:56:55 -0400

    Removing unused Logger class

--- core/src/main/java/com/ziodyne/sometrpg/logging/Logger.java
+++
@@ -1,8 +0,0 @@
-package com.ziodyne.sometrpg.logging;
-
-public interface Logger {
-  public void log(String msg);
-  public void error(String msg);
-  public void error(String msg, Throwable throwable);
-  public void debug(String msg);
-}

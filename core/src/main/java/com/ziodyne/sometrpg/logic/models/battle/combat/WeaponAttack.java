cbb82b03fd3e981e04ececbd11fae8912edf737c
Ryan Gee <ryan.cv.gee@gmail.com>
2014-08-23 15:54:49 -0400

    weapon attacks now reflect the gdoc forumlas

--- core/src/main/java/com/ziodyne/sometrpg/logic/models/battle/combat/WeaponAttack.java
+++ core/src/main/java/com/ziodyne/sometrpg/logic/models/battle/combat/WeaponAttack.java
@@ -1,5 +1,8 @@
 package com.ziodyne.sometrpg.logic.models.battle.combat;
 
+import com.ziodyne.sometrpg.logic.models.*;
+import com.ziodyne.sometrpg.logic.models.Character;
+
 public class WeaponAttack implements Attack {
   @Override
   public int getRange() {
@@ -8,16 +11,25 @@ public class WeaponAttack implements Attack {
 
   @Override
   public int computeHitChance(Combatant attacker, Combatant defender) {
-    return 100;
+    Character attackingCharacter = attacker.getCharacter();
+    Character defendingCharacter = defender.getCharacter();
+
+    return attackingCharacter.getStat(Stat.SKILL)*3 - defendingCharacter.getStat(Stat.SPEED);
   }
 
   @Override
   public int computeCritChance(Combatant attacker, Combatant defender) {
-    return 0;
+    Character attackingCharacter = attacker.getCharacter();
+    Character defendingCharacter = defender.getCharacter();
+
+    return attackingCharacter.getStat(Stat.SKILL) - defendingCharacter.getStat(Stat.SPEED);
   }
 
   @Override
   public int computeDamage(Combatant attacker, Combatant defender) {
-    return 100;
+    Character attackingCharacter = attacker.getCharacter();
+    Character defendingCharacter = defender.getCharacter();
+
+    return attackingCharacter.getStat(Stat.STRENGTH) - defendingCharacter.getStat(Stat.DEFENCE);
   }
 }
